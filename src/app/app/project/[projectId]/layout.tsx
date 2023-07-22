6795ad5669846f3374d0df13e754f54a31031859
karl <karlgandhi360@gmail.com>
2023-07-22 10:28:07 +0100

    added tooltips

--- src/app/app/project/[projectId]/layout.tsx
+++ src/app/app/project/[projectId]/layout.tsx
@@ -3,11 +3,17 @@ import { redirect } from "next/navigation";
 import { Search } from "@/components/search";
 import { Sidebar } from "@/components/sidebar";
 import { ThemeToggle } from "@/components/theme-toggle";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { UserNav } from "@/components/user-nav";
 import { authOptions } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { getCurrentUser } from "@/lib/session";
-import { Menu, MenuSquare } from "lucide-react";
+import { Menu } from "lucide-react";
 
 export const revalidate = 0;
 
@@ -38,18 +44,33 @@ export default async function RootLayout({
     <>
       {modal}
       <div>
-        <div className="flex h-16 items-center px-4">
-          <Menu strokeWidth={2} viewBox="0 0 24 24" className="mr-2 h-6 w-6" />
-          <Search />
-          <div className="ml-auto flex items-center space-x-4">
-            <ThemeToggle />
-            <UserNav />
+        <TooltipProvider>
+          <div className="flex h-16 items-center px-4">
+            <Tooltip>
+              <TooltipTrigger asChild disabled>
+                <Menu
+                  strokeWidth={2}
+                  viewBox="0 0 24 24"
+                  className="mr-2 h-6 w-6"
+                />
+              </TooltipTrigger>
+              <TooltipContent>
+                <p> Not yet implemented </p>
+              </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <Search />
+            </Tooltip>
+            <div className="ml-auto flex items-center space-x-4">
+              <ThemeToggle />
+              <UserNav />
+            </div>
           </div>
-        </div>
-        <div className="grid md:grid-cols-4 lg:grid-cols-5">
-          <Sidebar className="hidden md:block" projects={projects} />
-          <div className="col-span-3 lg:col-span-4">{children}</div>
-        </div>
+          <div className="grid md:grid-cols-4 lg:grid-cols-5">
+            <Sidebar className="hidden md:block" projects={projects} />
+            <div className="col-span-3 lg:col-span-4">{children}</div>
+          </div>
+        </TooltipProvider>
       </div>
     </>
   );
